3b3aac0a3354e8bbd543e78c04f4e573bf486415
Ben <bh00527@surrey.ac.uk>
2020-10-15 18:09:48 +0100

    Added Testing

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,8 +2,11 @@ require 'test_helper'
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get home_home_url
+    get root_url
     assert_response :success
+    assert_select'title', 'My Notes'
+    assert_select'h1', 'My Notes'
+    assert_select'p', 'Welcome to my notes site!'
   end
 
 end
