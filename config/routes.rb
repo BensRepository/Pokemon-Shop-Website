9a7d21ecfbe4ce8c8488194fd05682350c85df09
Ben Hodges <thebenhodges@gmail.com>
2020-11-27 14:51:00 +0000

    log in system and fourm generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+  resources :posts
   resources :products
   root 'home#home'
   get 'contact', to: 'home#contact'
   get 'products', to: 'products#index'
+  get 'fourm', to: 'posts#index'
   post 'request_contact', to: 'home#request_contact'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
